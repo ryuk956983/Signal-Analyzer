95a845a7e9f86dab52e6af77e0202def87552dfd
ryuk956983 <vermaanurag550@gmail.com>
2025-10-23 21:26:17 +0530

    keepalive

--- server/app.js
+++ server/app.js
@@ -89,6 +89,10 @@ If invalid, respond only with:
 
 });
 
+app.get("/api/keepalive", (req, res) => {
+    res.status(200).json({ message: "Server is alive" });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
